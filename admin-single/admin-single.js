0ff6bccc59af40b9a4208584174542cf3e9ec2b8
Olatunde Sodiq <olatundesodiq@gmail.com>
2019-06-24 07:37:28 +0100

    Create admin single job page where admin can post, delete and update a job

---
+++ admin-single/admin-single.js
@@ -0,0 +1,41 @@
+$(function(){
+    $.ajax({
+        method:"GET",
+        url: `http://localhost:3000/jobs/${localStorage.getItem("jobId")}`
+    })
+    .done(function(data){
+        $(".contain").html(`
+        <div class="card card-body">
+        <div class="job-header card card-body">
+            <h5 class="pb-2">${data.company}</h5>
+            <p>${data.title}</p>
+            <p>${data.location}</p>
+            <p>${data.category}</p>
+        </div>
+        <div class="job-summary card card-body">
+            <h5 class="pb-2">Job Summary</h5>
+            <p>${data.summary}</p>
+        </div>
+        <div class="job-description card card-body">
+                <h4 class="pb-2">Job Description</h4>
+                <p>
+                    ${data.description}
+                </p>
+                <div class="updateDelete">
+                <a href="#" class="update btn btn-info btn-lg text-right" id="${data.id}">UPDATE</a>
+                <a href="/delete/delete.html" class="delete btn btn-danger btn-lg" id="${data.id}">DELETE</a>
+                </div>
+            </div>
+           
+        </div>
+      
+     
+        `)
+
+        $(".delete").click(function(e){
+            localStorage.setItem("jobId", this.id)
+        })
+    })
+   
+
+})
