0f394ab22a17f08cf44e4a33e342ea7a8c0a3af7
Olatunde Sodiq <olatundesodiq@gmail.com>
2019-06-24 06:43:28 +0100

    Create read more js file

---
+++ single-page/single.js
@@ -0,0 +1,30 @@
+
+
+$.ajax({
+    method:"GET",
+    url: `http://localhost:3000/jobs/${localStorage.getItem("jobId")}`
+})
+.done(function(data){
+    $(".contain").html(`
+    <div class="card card-body">
+    <div class="job-header card card-body">
+        <h5 class="pb-2">${data.company}</h5>
+        <p>${data.title}</p>
+        <p>${data.location}</p>
+        <p>${data.category}</p>
+    </div>
+    <div class="job-summary card card-body">
+        <h5 class="pb-2">Job Summary</h5>
+        <p>
+            Managing the day to day marketing activities of the organisation and long term marketing strategy for the company.
+        </p>
+    </div>
+    <div class="job-description card card-body">
+            <h4 class="pb-2">Job Description</h4>
+            <p>
+                ${data.description}
+            </p>
+        </div>
+</div>
+    `)
+})
