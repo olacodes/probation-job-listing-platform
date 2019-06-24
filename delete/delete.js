d3eef4b04c9f9d16c2fce2f59278e18484776a03
Olatunde Sodiq <olatundesodiq@gmail.com>
2019-06-24 07:46:15 +0100

    Create a delete function

---
+++ delete/delete.js
@@ -0,0 +1,9 @@
+$(function(){
+    $.ajax({
+        method:"DELETE",
+        url:`http://localhost:3000/jobs/${localStorage.getItem("jobId")}`
+    })
+    .done(function(e){
+        window.location = "/admin/admin.html"
+    })
+})
