559e1c56f8e1f15e08f858ac6237c3b309ae764f
Olatunde Sodiq <olatundesodiq@gmail.com>
2019-06-26 09:16:03 +0100

    Correct jquery source on delete page

--- delete/delete.js
+++ delete/delete.js
@@ -4,6 +4,6 @@ $(function(){
         url:`http://localhost:3000/jobs/${localStorage.getItem("jobId")}`
     })
     .done(function(e){
-        window.location = "/admin/admin.html"
+        window.location = "../admin/admin.html"
     })
 })
